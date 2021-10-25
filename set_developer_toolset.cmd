3eea7a30c3c171875587ce6ff68261754e72b30a
Ernad Husremović <hernad@bring.out.ba>
2021-10-25 15:59:45 +0200

    c:\dev\F18_conan_deploy

--- set_developer_toolset.cmd
+++ set_developer_toolset.cmd
@@ -1,44 +1,101 @@
+@ECHO OFF
 
-@echo off
+REM set ARCH=x64
 
-set ARCH=x64
+echo %1
+
+if '%1'=='' (
+    set ARCH=x64
+) else (
+     set ARCH=x86
+)
+
+IF NOT EXIST c:\Python (
+   echo As administrator create symbolic link:
+   echo mklink /D c:\Python %USERPROFILE%\dev\Python
+   goto :eof
+)
+
+IF NOT EXIST c:\dev (
+   echo As administrator create symbolic link:
+   echo mklink /D c:\dev %USERPROFILE%\dev
+   goto :eof
+)
+
+echo ARCH=%ARCH%
 
 set ROOT=%USERPROFILE%\dev
 
 echo cygwin64
 set PATH=%ROOT%\cygwin64\bin;%PATH%
 
-echo java
-set JAVA_HOME=%ROOT%\AdoptOpenJDK\jdk-11
+goto :%ARCH%
 
-echo harbour
-set HARBOUR_HOME=%ROOT%\harbour\%ARCH%\harbour
+:x64
 
-echo python37
-set PYTHON_HOME=%ROOT%\Python\python37
+    echo java x64
+    set JAVA_HOME=%ROOT%\AdoptOpenJDK\jdk-11
+    echo harbour
+    set HARBOUR_HOME=%ROOT%\harbour\%ARCH%\harbour
 
-set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%PATH%
+    echo python37 x64
+    set PYTHON_HOME=%ROOT%\Python\Python37
+    set PYTHONPATH=%PYTHON_HOME%;%PYTHON_HOME%\DLLs;%PYTHON_HOME%\Lib;%PYTHON_HOME%\Lib\site-packages
+    set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%PATH%
+    set PATH=%HARBOUR_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+    python --version
+    pip --version
+
+    set CMAKE_HOME=%ROOT%\Cmake
+
+    echo cmake
+    set PATH=%CMAKE_HOME%\bin;%PATH%
+
+    cmake --version
+    echo bazel
+    set PATH=%ROOT%\bazel;%PATH%
+
+    set NODE_HOME=%ROOT%\nodejs\12
+    set PATH=%NODE_HOME%;%PATH%
+
+    node --version
+    java -version
+    %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+    goto :eof
 
 
-set PATH=%HARBOUR_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+:x86
 
-python --version
-pip --version
+    echo java x86
+    set JAVA_HOME=%ROOT%\AdoptOpenJDK\%ARCH%\jdk-11.0.7.10-hotspot
+    echo harbour
+    set HARBOUR_HOME=%ROOT%\harbour\%ARCH%\harbour
 
-set CMAKE_HOME=%ROOT%\Cmake
+    echo python37 x86
+    set PYTHON_HOME=%ROOT%\Python\Python37-32
+    set PYTHONPATH=%PYTHON_HOME%;%PYTHON_HOME%\DLLs;%PYTHON_HOME%\Lib;%PYTHON_HOME%\Lib\site-packages
+    set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%PATH%
+    set PATH=%HARBOUR_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+    python --version
+    pip --version
 
-echo cmake
-set PATH=%CMAKE_HOME%\bin;%PATH%
+    set CMAKE_HOME=%ROOT%\Cmake
 
-cmake --version
+    echo cmake
+    set PATH=%CMAKE_HOME%\bin;%PATH%
 
-echo bazel
-set PATH=%ROOT%\bazel;%PATH%
+    cmake --version
+    echo bazel
+    set PATH=%ROOT%\bazel;%PATH%
 
-set NODE_HOME=%ROOT%\nodejs\12
-set PATH=%NODE_HOME%;%PATH%
+    set NODE_HOME=%ROOT%\nodejs\12-32
+    set PATH=%NODE_HOME%;%PATH%
 
-node --version
+    node --version
+    java -version
+    %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+    goto :eof
 
+:eof
 
-%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+echo -- end --
